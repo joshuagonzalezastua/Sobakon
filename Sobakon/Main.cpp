30391c9361e555e573bc34376a69a6ce40a780bd
joshua gonzalez astua <josh.6.4.steve@hotmail.com>
2019-06-09 23:14:03 -0600

    first commit

---
+++ Sobakon/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Window.h"
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+using namespace std;
+int main()
+{
+	Window window;
+	window.update();
+	return 0;
+}
